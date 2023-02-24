eded207aa6ebad52b8a6fdba882740e8f41cb3ce
hojun <hjlee9303@gmail.com>
2023-02-24 23:05:24 +0900

    Feat(pjt5): '예매자 정보 입력' 기능 구현  1. reserve.html 파일을 jsp로 구현. 예매일을 현재 날짜 + (1~5일 랜덤값) 로 설정  2. 연락처 번호는 핸드폰, 집 전화 형식만 입력이 가능하도록 변경

--- src/main/webapp/js/reserve.js
+++ src/main/webapp/js/reserve.js
@@ -1,13 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
     BtnBackSetter.setBtnBack();
+
     const ticketTypeCount = 4;
     let quantityList = document.querySelectorAll(".qty");
     for (let i = 0; i < ticketTypeCount; i++) {
-        quantityList[i].addEventListener("click", function(evt) {
+        quantityList[i].addEventListener("click", function (evt) {
             let countBtn = new CountBtn();
             countBtn.countTicket(evt);
         });
     }
+
+    BookingForm.init();
 });
 
 const BtnBackSetter = {
@@ -32,7 +35,6 @@ CountBtn.prototype = {
         } else if (evt.target.className.includes("ico_plus3")) {
             this.setPlusCount(evt);
         }
-
     },
     setMinusCount: function (evt) {
         let currentCount = evt.target.nextElementSibling.value;
@@ -50,6 +52,7 @@ CountBtn.prototype = {
         let price = evt.currentTarget.getElementsByClassName('price')[0].innerText.replace(',', '');
         let totalPrice = evt.currentTarget.getElementsByClassName('total_price')[0].innerHTML;
         evt.currentTarget.getElementsByClassName('total_price')[0].innerHTML = Number(totalPrice) - Number(price);
+        this.countTotalTicket(0, 1);
     },
     setPlusCount: function (evt) {
         let currentCount = evt.target.previousElementSibling.value;
@@ -64,5 +67,36 @@ CountBtn.prototype = {
         let price = evt.currentTarget.getElementsByClassName('price')[0].innerText.replace(',', '');
         let totalPrice = evt.currentTarget.getElementsByClassName('total_price')[0].innerHTML;
         evt.currentTarget.getElementsByClassName('total_price')[0].innerHTML = Number(totalPrice) + Number(price);
+        this.countTotalTicket(1, 0);
+    },
+    countTotalTicket: function (addCount, minusCount) {
+        let totalCount = document.getElementById('totalCount').innerHTML;
+        document.getElementById('totalCount').innerHTML = Number(totalCount) + addCount - minusCount;
+    }
+}
+
+/*
+    예매자 정보 검증
+     - 휴대전화, 집전화 검증
+*/
+const BookingForm = {
+    init: function () {
+        let bookingForm = document.querySelector('.form_horizontal');
+        bookingForm.addEventListener("change", this.checkBookInput);
+
+        let bookInputs = document.querySelectorAll('.inline_control > input');
+        bookInputs.forEach(function (bookinput) {
+            bookinput.addEventListener('change', this.checkBookInput);
+        });
+    },
+    checkBookInput: function (evt) {
+        if (evt.target.id === 'tel') {
+            let isMatch = evt.target.value.match(/01[01789]-\d{3,4}-\d{4}|\d{2,3}-\d{3,4}-\d{4}$/);
+            if (isMatch === null) {
+                evt.target.value = '';
+                alert('옳바른 휴대전화 형식을 입력해주세요. \n예시) 010-0000-0000');
+                return;
+            }
+        }
     }
 }
